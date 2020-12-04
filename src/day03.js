f0aec5e6ed0a54d4f8fa3a662f67b8d07ca64f63
OrangeBacon <computer.backup.15@gmail.com>
2020-12-04 07:51:16 +0000

    Day 03 js oneliner

---
+++ src/day03.js
@@ -0,0 +1,9 @@
+console.log(
+    [[[3,1]],[[1,1],[3,1],[5,1],[7,1],[1,2]]]
+    .map(c=>c
+        .reduce((p,x)=>p*require("fs")
+            .readFileSync('data/day3.txt', 'utf8')
+            .split("\r\n")
+            .filter(x=>x.length>0)
+            .map(x=>x.split(''))
+            .reduce((p,c,i)=>[(i%x[1]==0)?(c[p[1]]=='#')+p[0]:p[0],(i%x[1]==0)?(p[1]+x[0])%c.length:p[1]],[0,0])[0],1)));
