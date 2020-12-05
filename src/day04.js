0280cad3ac260482ed76666a8908b46a6decce26
OrangeBacon <computer.backup.15@gmail.com>
2020-12-05 00:34:24 +0000

    Golfed day4js

--- src/day04.js
+++ src/day04.js
@@ -3,14 +3,13 @@ console.log(
     .readFileSync('data/day4.txt', 'utf-8')
     .split("\n\n")
     .map(x=>x.split(/[ \n]/).map(x=>x.split(':')))
-    .filter(x=>x.length==8||(x.length==7&&!x.find(y=>y[0]=="cid")))
-    .map(x=>[1, x.map(([k,v])=>
-        k=="byr"?/19[2-9][0-9]|200[0-2]/.test(v):
-        k=="iyr"?/201[0-9]|2020/.test(v):
-        k=="eyr"?/202[0-9]|2030/.test(v):
-        k=="hgt"?/^(?:1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)$/.test(v):
-        k=="hcl"?/#[0-9a-f]{6}/.test(v):
-        k=="ecl"?/amb|blu|brn|gry|grn|hzl|oth/.test(v):
-        k=="pid"?/^[0-9]{9}$/.test(v):
-        true).reduce((a,b)=>a&b,1)])
-    .reduce((p,v)=>[p[0]+v[0],p[1]+v[1]], [0,0]));
+    .filter(x=>x.length==8-!x.some(y=>y[0]=="cid"))
+    .reduce(([a,b],x)=>[a+1, b+x.map(([k,v])=>
+        ({byr:/19[2-9][0-9]|200[0-2]/,
+        iyr:/201[0-9]|2020/,
+        eyr:/202[0-9]|2030/,
+        hgt:/^(?:1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)$/,
+        hcl:/#[0-9a-f]{6}/,
+        ecl:/amb|blu|brn|gry|grn|hzl|oth/,
+        pid:/^[0-9]{9}$/,
+        cid:/.*/})[k].test(v)).reduce((a,b)=>a&b)],[0,0]));
