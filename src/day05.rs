bbf926ed72f9f529efd487f62d398294bfe43b05
OrangeBacon <computer.backup.15@gmail.com>
2020-12-05 07:06:18 +0000

    cleaned up day5 + wrote shorter solution - converts to binary, rather than iteration

--- src/day05.rs
+++ src/day05.rs
@@ -3,20 +3,16 @@ use std::cmp::{max};
 pub fn day05(input: String) {
     let lines : Vec<_> = input.lines().collect();
 
-    let mut highest = 0;
+    let mut seats = [[false; 8]; 128];
 
-    let mut seats = [[(false, 0); 8]; 128];
-
-    for line in lines {
+    let highest = lines.iter().fold(0, |acc, &line| {
         let mut row = 0;
         let mut col = 0;
-        let mut row_size = 128;
-        let mut col_size = 8;
+        let mut row_size = 64;
+        let mut col_size = 4;
         for c in line.chars() {
             match c {
-                'F' => { // lower
-                    row_size /= 2;
-                }
+                'F' => row_size /= 2,
                 'B' => { // upper
                     row += row_size;
                     row_size /= 2;
@@ -25,35 +21,43 @@ pub fn day05(input: String) {
                     col += col_size;
                     col_size /= 2;
                 }
-                'L' => { // lower
-                    col_size /= 2;
-                }
+                'L' => col_size /= 2,
                 _ => panic!()
             }
         }
-        seats[row/2][col/2] = (true, row/2 * 8 + col/2);
-        highest = max(highest, row/2 * 8 + col/2);
-    }
+        seats[row][col] = true;
+        max(acc, row * 8 + col)
+    });
+
+    println!("{}", highest);
 
     let mut found = false;
-    let mut id = 0;
-    'out: for (y, row) in seats.iter().enumerate() {
+    for (y, row) in seats.iter().enumerate() {
         if !found {
-            for seat in row.iter() {
-                if (*seat).0 {
-                    found = true;
-                }
-            }
+            found = row.iter().fold(false, |a,x|a|x);
         } else {
-            for (x, seat) in row.iter().enumerate() {
-                if !(*seat).0 {
-                    id = y * 8 + x;
-                    break 'out;
-                }
+            let res = row.iter().enumerate().fold(0, |_,(x,v)|if *v{0} else {y*8+x});
+            if res > 0 {
+                println!("{}", res);
+                break;
             }
         }
     }
 
-    println!("{}", highest);
-    println!("{}", id);
+    //day05v2(input);
+}
+
+use std::collections::HashSet;
+
+fn day05v2(input: String) {
+    let lines : HashSet<_> = input.lines().map(|v|
+        i32::from_str_radix(&v.replace("F","0").replace("B", "1").replace("L", "0").replace("R", "1"), 2)
+        .unwrap()
+    ).collect();
+    println!("{}", lines.iter().max().unwrap());
+
+    let min = *lines.iter().min().unwrap();
+    let max = *lines.iter().max().unwrap();
+    let ids : HashSet <_> = (min..max).collect();
+    println!("{}", (&ids - &lines).iter().next().unwrap());
 }
