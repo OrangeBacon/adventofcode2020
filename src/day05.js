f01a991f0e8bb981a8f781c0b68f19bfeb9e0c32
OrangeBacon <computer.backup.15@gmail.com>
2020-12-06 04:42:21 +0000

    Golfed js day5 more

--- src/day05.js
+++ src/day05.js
@@ -1,10 +1,8 @@
 console.log(
     require("fs")
-    .readFileSync("data/day5.txt", "utf-8")
+    .readFileSync("data/BigBigDay5.txt", "utf-8")
+    .trim()
     .split("\n")
-    .filter(x=>x.length>0)
-    .map(x=>parseInt(x.trim().replace(/[FBLR]/g,x=>({F:0,B:1,L:0,R:1})[x]),2))
+    .map(x=>parseInt(x.trim().replace(/[FBLR]/g,x=>+"BR".includes(x)),2))
     .sort((a,b)=>a-b)
-    .reduce((a,v,i,t)=>[t,t[0],t.reverse()[0], t[0]])
-    .reduce((a,v,i,t)=>[t[2],[...Array(t[2]-t[1]).keys()].map(x=>x+t[1]),t[0]])
-    .reduce((a,v,i,t)=>[t[0],t[1].filter(x=>!t[2].includes(x))[0]]));
+    .reduce(([p,m],v)=>[v,v-p-2?m:v-1],[]));
