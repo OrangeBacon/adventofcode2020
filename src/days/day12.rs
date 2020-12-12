f82390879fc12e25bbba10a0270734e08100e2c1
OrangeBacon <computer.backup.15@gmail.com>
2020-12-12 06:52:56 +0000

    Tidy up day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -2,61 +2,93 @@ use anyhow::Result;
 use libaoc::{aoc, AocResult};
 use std::time::Instant;
 
+enum Movement {
+    North,
+    South,
+    East,
+    West,
+    Left,
+    Right,
+    Forward,
+}
+use Movement::*;
+
+impl Movement {
+    fn new(name: char) -> Self {
+        match name {
+            'N' => North,
+            'S' => South,
+            'E' => East,
+            'W' => West,
+            'L' => Left,
+            'R' => Right,
+            'F' => Forward,
+            _ => panic!(),
+        }
+    }
+}
+
 fn rotate_one(current: (i32, i32)) -> (i32, i32) {
     match current {
         (1, 0) => (0, 1),
         (0, 1) => (-1, 0),
         (-1, 0) => (0, -1),
         (0, -1) => (1, 0),
-        _ => panic!()
+        _ => panic!(),
     }
 }
 
-fn rotate(current: (i32,i32), left: bool, amount: i32) -> (i32, i32) {
-    let amount = if left {
-        amount
-    } else { 4 - amount };
+fn rotate(current: (i32, i32), left: bool, amount: i32) -> (i32, i32) {
+    let amount = if left { amount } else { 4 - amount };
 
     let mut res = current;
-    for _ in 0..amount { res = rotate_one(res); }
+    for _ in 0..amount {
+        res = rotate_one(res);
+    }
     res
 }
 
 fn waypoint(left: bool, amount: i32, x: i32, y: i32) -> (i32, i32) {
-    let direction = rotate((1,0), left, amount);
+    let direction = rotate((1, 0), left, amount);
     match direction {
         (0, 1) => (-y, x),
         (-1, 0) => (-x, -y),
         (0, -1) => (y, -x),
-        _ => panic!()
+        _ => panic!(),
     }
 }
 
 #[aoc("2297", "89984")]
 pub fn solve(input: String) -> Result<AocResult> {
     let parse = Instant::now();
-    let input: Vec<_> = input.trim().lines().map(|x| {
-        (x.chars().nth(0).unwrap(), x[1..].parse::<i32>().unwrap())
-    }).collect();
+    let input: Vec<_> = input
+        .trim()
+        .lines()
+        .map(|x| {
+            (
+                Movement::new(x.chars().nth(0).unwrap()),
+                x[1..].parse::<i32>().unwrap(),
+            )
+        })
+        .collect();
     let parse = parse.elapsed().as_secs_f64();
 
     let t1 = Instant::now();
     let mut x = 0;
     let mut y = 0;
-    let mut direction = (1,0);
+    let mut direction = (1, 0);
     for (code, count) in &input {
         match code {
-            'N' => y += count,
-            'S' => y -= count,
-            'E' => x += count,
-            'W' => x -= count,
-            'L' => direction = rotate(direction, true, count/90),
-            'R' => direction = rotate(direction, false, count/90),
-            'F' => {
+            North => y += count,
+            South => y -= count,
+            East => x += count,
+            West => x -= count,
+            Left => direction = rotate(direction, true, count / 90),
+            Right => direction = rotate(direction, false, count / 90),
+            Forward => {
                 x += count * direction.0;
                 y += count * direction.1;
             }
-            _ => panic!()
         }
     }
     let part1 = x.abs() + y.abs();
@@ -69,17 +101,16 @@ pub fn solve(input: String) -> Result<AocResult> {
     let mut waypoint_y = 1;
     for (code, count) in input {
         match code {
-            'N' => waypoint_y += count,
-            'S' => waypoint_y -= count,
-            'E' => waypoint_x += count,
-            'W' => waypoint_x -= count,
-            'L' => (waypoint_x,waypoint_y) = waypoint(true, count/90, waypoint_x, waypoint_y),
-            'R' => (waypoint_x,waypoint_y) = waypoint(false, count/90, waypoint_x, waypoint_y),
-            'F' => {
+            North => waypoint_y += count,
+            South => waypoint_y -= count,
+            East => waypoint_x += count,
+            West => waypoint_x -= count,
+            Left => (waypoint_x, waypoint_y) = waypoint(true, count / 90, waypoint_x, waypoint_y),
+            Right => (waypoint_x, waypoint_y) = waypoint(false, count / 90, waypoint_x, waypoint_y),
+            Forward => {
                 x += waypoint_x * count;
                 y += waypoint_y * count;
             }
-            _ => panic!()
         }
     }
     let part2 = x.abs() + y.abs();
