3a1411af739f396bb354ef0c088b92b7a5d42209
OrangeBacon <computer.backup.15@gmail.com>
2020-12-15 16:33:33 +0000

    Increased speed of day 15

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,34 +1,35 @@
 use anyhow::Result;
-use hashbrown::HashMap;
 use libaoc::{aoc, AocResult, Timer};
 
-fn run(numbers: &[usize], max: usize) -> usize {
+fn run(numbers_map: &mut [(i32, usize, usize)], numbers: &[usize], max: usize) -> usize {
     let mut spoken = 0;
     let len = numbers.len();
-    let mut numbers_map = HashMap::new();
     for i in 0..max {
         let num = numbers[i % len];
         if i < numbers.len() {
-            numbers_map.insert(num, (1, i, 0));
+            numbers_map[num] = (1, i, 0);
             spoken = num;
-        } else if let Some(last) = numbers_map.get_mut(&spoken) {
+        } else if numbers_map[spoken] != (0, 0, 0) {
+            let last = &mut numbers_map[spoken];
             if last.0 == 1 {
                 spoken = 0;
-                let num = numbers_map.get_mut(&0).unwrap();
+                let num = &mut numbers_map[0];
                 num.0 += 1;
                 num.2 = num.1;
                 num.1 = i;
             } else {
                 spoken = last.1 - last.2;
-                let temp = (1, i, 0);
-                let mut num = *numbers_map.get(&spoken).unwrap_or(&temp);
+                let mut num = numbers_map[spoken];
+                if num == (0, 0, 0) {
+                    num = (1, i, 0);
+                }
                 num.0 += 1;
                 num.2 = num.1;
                 num.1 = i;
-                numbers_map.insert(spoken, num);
+                numbers_map[spoken] = num;
             }
         } else {
-            numbers_map.insert(spoken, (1, i, 0));
+            numbers_map[spoken] = (1, i, 0);
             spoken = 0;
         }
     }
@@ -38,12 +39,14 @@ fn run(numbers: &[usize], max: usize) -> usize {
 #[aoc("1015", "201")]
 pub fn solve(timer: &mut Timer, _input: String) -> Result<AocResult> {
     let numbers = vec![19, 0, 5, 1, 10, 13];
+    let mut map1 = vec![(0, 0, 0); 2020];
+    let mut map2 = vec![(0, 0, 0); 30000000];
     timer.lap("Parse");
 
-    let part1 = run(&numbers, 2020);
+    let part1 = run(&mut map1, &numbers, 2020);
     timer.lap("Part 1");
 
-    let part2 = run(&numbers, 30000000);
+    let part2 = run(&mut map2, &numbers, 30000000);
     timer.lap("Part 2");
 
     Ok(AocResult::new(part1, part2))
